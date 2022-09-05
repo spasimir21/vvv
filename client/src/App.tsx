78834b4b935ff53b4387ad393233c57cba3c1cbd
Vladimir Pasev <vladigpasev@gmail.com>
2022-09-06 00:35:51 +0300

    bug fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import './app.scss';
 import { LandingPage } from './pages/LandingPage/LandingPage';
 import { Register } from './pages/Register/Register';
 import { Login } from './pages/Login/Login';
