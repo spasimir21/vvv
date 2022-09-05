0d2f9a5c7499799d8226725a46de760db75e865a
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-05 16:05:12 +0300

    Home page wideframe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
 import './app.scss';
+import React from 'react'
+import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>{count}</p>
-      <button className='button' onClick={() => setCount(count + 1)}>
-        Add
-      </button>
+      <HomePage/>
     </>
   );
 }
---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import './HomePage.scss';
+
+export const HomePage = () => {
+    return (
+        <section className="home-page">
+            <section className="image">
+
+            </section>
+            <h1>Welcome to App Name</h1>
+            <button>Get started</button>
+        </section>
+    )
+}
