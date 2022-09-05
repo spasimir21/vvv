3385ff6a2cec90b1c65a8ac672995ab7bf39291b
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-05 23:44:24 +0300

    Sashko feels bad

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,12 @@ import './app.scss';
 import { LandingPage } from './pages/LandingPage/LandingPage';
 import { Register } from './pages/Register/Register';
 import { Login } from './pages/Login/Login';
+import { ProductsPage } from './pages/ProductsPage/ProductsPage';
 
 function App() {
   return (
     <>
-      <LandingPage/>
+      <ProductsPage/>
     </>
   );
 }
