ec0cd5219e5544e35aa72dc3d209199a7d68043c
Vladimir Pasev <vladigpasev@gmail.com>
2022-09-05 21:00:15 +0300

    Footer (почти), login, register,

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import React from 'react';
 import './app.scss';
-import { Homepage } from './pages/HomePage/HomePage';
+import { HomePage } from './pages/HomePage/HomePage';
 import { Register } from './pages/Register/Register';
+import { Login } from './pages/Login/Login';
 
 function App() {
   return (
     <>
-      <HomePage/>
+      <Register />
     </>
   );
 }
---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import React from 'react';
+import './Login.scss';
+
+function Login() {
+  return (
+    <>
+     <center> <p className="appnamelogo">FOODY</p>
+      <p className="createaccount">Wellcome back</p>
+      <form>
+        <input type="text" className="username" placeholder="Username"/> <br />
+        <input type="password" className="password" placeholder="Password"/><br />
+        <p className="signin">Already have an account? <a href="#">Sign in</a></p>
+        <input type="submit" value="Sign in" />
+      </form>
+      </center>
+    </>
+  );
+}
+
+export { Login };
