20c4fd8f186ca92ebc30726b47a525f8c48cc135
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-05 21:05:20 +0300

    React router dom

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
