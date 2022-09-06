82c1c4a938c0e9a78faf55705298285a1509b1d2
Spasimir Pavlov <spasimir.pavlov@gmail.com>
2022-09-06 09:54:26 +0300

    Added useRequest hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import { LandingPage } from './pages/LandingPage/LandingPage';
 import { Register } from './pages/Register/Register';
 import { Login } from './pages/Login/Login';
-import { ProductsPage } from './pages/ProductsPage/ProductsPage';
 
 function App() {
   return (
     <>
-      <ProductsPage />
+      <LandingPage />
     </>
   );
 }
